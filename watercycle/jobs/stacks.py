4af8538c487053d55f980bb50dda9d76a99ddb9e
erebuseternal <marcelsanders96@gmail.com>
2024-09-19 13:51:43 +0000

    public ip

--- watercycle/jobs/stacks.py
+++ watercycle/jobs/stacks.py
@@ -47,7 +47,10 @@ class BatchJobStack(Stack):
             ephemeral_storage=ephemeral_storage_property,
             execution_role_arn=f"arn:aws:iam::{config['account']}:role/{job_role_name}",
             job_role_arn=f"arn:aws:iam::{config['account']}:role/{job_role_name}",
-            fargate_platform_configuration=fargate_platform_configuration_property
+            fargate_platform_configuration=fargate_platform_configuration_property,
+            network_configuration=batch.CfnJobDefinition.NetworkConfigurationProperty(
+                assign_public_ip="ENABLED"
+            )
         )
 
         _batch_job = batch.CfnJobDefinition(
