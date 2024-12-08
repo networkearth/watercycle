38da4989a68c4db4cd7e5c9c6eafd3ec97b15c58
erebuseternal <marcelsanders96@gmail.com>
2024-12-08 14:16:50 +0000

    reasonable worker sizes

--- watercycle/spark/submit.py
+++ watercycle/spark/submit.py
@@ -21,7 +21,7 @@ def submit_spark_job(config):
 
     execution_role_arn = f"arn:aws:iam::{config['account']}:role/{get_role_name(config)}"
 
-    resource_conf = "--conf spark.executor.cores=1 --conf spark.executor.memory=4g --conf spark.driver.cores=1 --conf spark.driver.memory=4g --conf spark.executor.instances=1 --conf spark.dynamicAllocation.enabled=true --conf spark.dynamicAllocation.minExecutors=1"
+    resource_conf = "--conf spark.executor.cores=4 --conf spark.executor.memory=16g --conf spark.driver.cores=4 --conf spark.driver.memory=16g --conf spark.executor.instances=1 --conf spark.dynamicAllocation.enabled=true --conf spark.dynamicAllocation.minExecutors=1"
     
     venv_key = f"spark-venv/{config['application_name']}/{config['venv']}.tar.gz"
     venv_conf = f"--conf spark.archives=s3://{bucket}/{venv_key}#environment --conf spark.emr-serverless.driverEnv.PYSPARK_DRIVER_PYTHON=./environment/bin/python --conf spark.emr-serverless.driverEnv.PYSPARK_PYTHON=./environment/bin/python --conf spark.executorEnv.PYSPARK_PYTHON=./environment/bin/python"
